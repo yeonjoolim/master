a2758ca358fc5ba95dabb4ef2bc6ea18ea1ac085
yeonjoolim <chocobeeyj@gmail.com>
2020-10-28 12:09:53 +0900

    pv shell update

--- shell/5.grafana-delete.sh
+++ shell/5.grafana-delete.sh
@@ -3,5 +3,5 @@
 cd /home/guest/sju/master/grafana
 
 sudo kubectl delete -f grafana.yaml -n sju
-sudo kubectl delete -f mysql.yaml -n sju
-sudo kubectl delete -f mysql-pv.yaml -n sju
+sudo kubectl delete -f mysql.yaml -n sju --grace-period=0 --force
+sudo kubectl delete -f mysql-pv.yaml -n sju --grace-period=0 --force
